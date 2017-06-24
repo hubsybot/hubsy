77c04e06ff436fd6ad145f69086741aa8f496f90
Andrew Puch <andrewpuch@gmail.com>
2017-06-24 10:30:39 -0400

    Great example for andrew all set and ready to go...need to move on

--- deals_in_stage.js
+++ deals_in_stage.js
@@ -1,37 +1,67 @@
+// Reference
+// https://api.hubapi.com/deals/v1/deal/paged?hapikey=[Hubspot_API_Key]&properties=dealstage&properties=hubspot_owner_id
+
 // Include the hubspot helper.
 const helper = require(__dirname + "/hubspot_helper");
 
 // Handler for the lambda function.
 exports.handler = (event, context, callback) => {
-    var id    = null;
-    var stage = event.currentIntent.slots.stage;
+    // Stage information.
+    var stage_id = null;
+    var stage    = event.currentIntent.slots.stage;
+
+    // Sales information.
+    var sales       = null;
+    var sales_email = null;
 
     // It's not exactly clear what stage name could come back in the slot
     // so we can just see if it includes one of our predefined ones.
     if(stage.includes("discovery") === true) {
-        id = "appointmentscheduled";
+        stage_id = "appointmentscheduled";
     } else if(stage.includes("presentation") === true) {
-        id = "qualifiedtobuy";
+        stage_id = "qualifiedtobuy";
     } else if(stage.includes("quote") === true) {
-        id = "presentationscheduled";
+        stage_id = "presentationscheduled";
     } else if(stage.includes("negotite") === true) {
-        id = "decisionmakerboughtin";
+        stage_id = "decisionmakerboughtin";
     }
 
-    console.log(event.currentIntent.slots);
+    // If there is a sales slot configured check to see who it is if is none of the
+    // ones provided it was invalid and we can null the rest out.
+    if(event.currentIntent.slots.sales !== null) {
+        sales = event.currentIntent.slots.sales;
+
+        if(sales.includes("john") === true) {
+            sales_email = "johnswetzel@gmail.com";
+        } else if(sales.includes("andy") === true) {
+            sales_email = "andrewpuch@gmail.com";
+        } else {
+            sales       = null;
+            sales_email = null;
+        }
+    }
 
     // Create the request into hubspot.
-    helper.createRequest("/deals/v1/deal/paged?properties=dealstage", "GET", null).then((data) => {
+    helper.createRequest("/deals/v1/deal/paged?properties=dealstage&properties=hubspot_owner_id", "GET", null).then((data) => {
         var num_deals = 0;
+        var content   = "";
 
         // Loop through each of the deals and if one matches the id of the stage
         // then increase the counter.
         // @TODO this is paged so we will need to work with multiple pages.
         data.deals.forEach((deal) => {
-            if(deal.properties.dealstage.versions[0].value === id) {
+            if(deal.properties.dealstage.versions[0].value === stage_id && sales === null) {
+                ++num_deals;
+            } else if(deal.properties.dealstage.versions[0].value === stage_id && sales !== null && deal.properties.hubspot_owner_id.sourceId === sales_email) {
                 ++num_deals;
             }
         });
+
+        if(sales !== null) {
+            content = "There are " + num_deals + " in the " + stage + " stage assigned to " + sales;
+        } else {
+            content = "There are " + num_deals + " in the " + stage + " stage.";
+        }
 
         // Respond to Lex.
         callback(null, {
@@ -40,7 +70,7 @@ exports.handler = (event, context, callback) => {
                 "fulfillmentState" : "Fulfilled",
                 "message" : {
                     "contentType" : "PlainText",
-                    "content" : "There are " + num_deals + " in the " + stage + " stage."
+                    "content" : content
                 }
             }
         });
