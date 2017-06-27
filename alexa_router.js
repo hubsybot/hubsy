830cdc97a38249f43d056f4b4d5313c51874c638
Andrew Puch <andrewpuch@gmail.com>
2017-06-26 20:11:55 -0400

    Comments and failure allback

--- alexa_router.js
+++ alexa_router.js
@@ -1,3 +1,8 @@
+const lambda_helper  = require(__dirname + "/helpers/lambda_helper");
+
+// The Alexa router will take a look at the intent to figure out what it needs to
+// to do with it. To keep things common wether it's Lex or Alexa it will just require
+// the file it needs to and pass along the environment variables.
 exports.handler = (event, context, callback) => {
     var intent = null;
 
@@ -10,7 +15,10 @@ exports.handler = (event, context, callback) => {
             break;
     }
 
+    // If the intent was not found send back a failure.
     if(intent !== null) {
         intent.handler(event, context, callback);
+    } else {
+        lambda_helper.processCallback(callback, event, "Failed", "I am sorry but we could not process the request.");
     }
 };
