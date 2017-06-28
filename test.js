6b38ac805e7110894986948f2d9233219b4e8fca
Andrew Puch <andrewpuch@gmail.com>
2017-06-28 08:40:11 -0400

    test

---
+++ test.js
@@ -0,0 +1,19 @@
+const config         = require(__dirname + "/config/config.json");
+const hubspot_helper = require(__dirname + "/helpers/hubspot_helper");
+const similarity     = require("string-similarity");
+
+hubspot_helper.createRequest("/deals/v1/deal/paged?properties=dealstage&properties=dealname&properties=hubspot_owner_id", "GET", null).then((body) => {
+    var str = "give me the deal for house of worship church";
+
+    body.forEach((data) => {
+        data.deals.forEach((deal) => {
+            var percentage = Math.round(parseFloat(similarity.compareTwoStrings(str, deal.properties.dealname.value)) * 100) / 100;
+
+            if(percentage > 0.5) {
+                console.log(`FOUND (${percentage}): ${deal.properties.dealname.value}`);
+            } else {
+                console.log(`NO (${percentage}): ${deal.properties.dealname.value}`);
+            }
+        });
+    });
+});
