99a3dffd2189f183212faaecfdd68606f6927cfb
Andrew Puch <andrewpuch@gmail.com>
2017-07-11 00:29:28 -0400

    Commands are now sending cards based on slots to elicit

--- helpers/lambda_helper.js
+++ helpers/lambda_helper.js
@@ -1,3 +1,72 @@
+const config = require(__dirname + "/../config/config.json");
+
+// Local function for attaching a response card to an event.
+var createCard = (cards) => {
+    return {
+        contentType : "application/vnd.amazonaws.card.generic",
+        genericAttachments : cards
+    };
+};
+
+// Constant timeframe cards.
+var getTimeframeCards = (title) => {
+    var cards = [{ title : title, buttons : [ ] }];
+
+    config.timeframes.forEach((timeframe) => {
+        cards[0].buttons.push({ text : timeframe.name, value : timeframe.name.toLowerCase() });
+    });
+
+    return cards;
+};
+
+// Constant engagement cards.
+var getEngagmentCards = (title) => {
+    var cards = [{ title : title, buttons : [ ] }];
+
+    config.engagements.forEach((engagement) => {
+        cards[0].buttons.push({ text : engagement.name.charAt(0) + engagement.name.slice(1).toLowerCase(), value : engagement.name });
+    });
+
+    return cards;
+};
+
+// Constant stage cards.
+var getStageCards = (title) => {
+    var cards = [{ title : title, buttons : [ ] }];
+
+    config.stages.forEach((stage) => {
+        cards[0].buttons.push({ text : stage.name.charAt(0).toUpperCase() + stage.name.slice(1), value : stage.name });
+    });
+
+    return cards;
+};
+
+// Confirmation cards.
+var getConfirmationCards = (title) => {
+    var cards = [
+        {
+            title : title,
+            buttons : [
+                { text : "Yes", value : "yes" },
+                { text : "No", value : "no" }
+            ]
+        }
+    ];
+
+    return cards;
+};
+
+// Constant sales cards.
+var getSalesCards = (title) => {
+    var cards = [{ title : title, buttons : [ ] }];
+
+    config.sales_people.forEach((sales) => {
+        cards[0].buttons.push({ text : `${sales.first.charAt(0).toUpperCase()}${sales.first.slice(1)} ${sales.last.charAt(0).toUpperCase()}${sales.last.slice(1)}`, value : `${sales.first} ${sales.last}` });
+    });
+
+    return cards;
+};
+
 // Lex and Amazon Skills have slots in different ways. This helper will parse
 // them and return them to you. Agnostic of what the slot types are it will
 // map them properly and give you a common object back.
@@ -60,7 +129,7 @@ exports.processCallback = (callback, event, fulfillmentState, message) => {
     // Verify the developer passed a valid fulfillmentState.
     if(fulfillmentState !== "Failed" && fulfillmentState !== "Fulfilled") {
         fulfillmentState = "Failed";
-        message = "You have provided an invalid fulfillment state. It must be either Failed or Fulfilled.";
+        message          = "You have provided an invalid fulfillment state. It must be either Failed or Fulfilled.";
     }
 
     // Send back to Alexa Skill if there is an event.version. Otherwise
@@ -102,6 +171,8 @@ exports.processCallback = (callback, event, fulfillmentState, message) => {
 
 // Wrapper for making a validation callback to Lambda.
 exports.processValidation = (callback, event, slot_to_elicit, message) => {
+    var response = {};
+
     console.log(`Slot To Elicit: ${slot_to_elicit}`);
     console.log(`Message: ${message}`);
     console.log(`Validation Event: ${JSON.stringify(event)}`);
@@ -109,7 +180,7 @@ exports.processValidation = (callback, event, slot_to_elicit, message) => {
     if(event.messageVersion !== undefined) {
         console.log("Validating To Lex");
 
-        callback(null, {
+        response = {
             sessionAttributes : event.sessionAttributes,
             dialogAction : {
                 type : "ElicitSlot",
@@ -121,13 +192,42 @@ exports.processValidation = (callback, event, slot_to_elicit, message) => {
                     content : message
                 },
             },
-        });
+        };
+
+        // Create the cards based on the slot to elicit.
+        if(slot_to_elicit === "stage") {
+            response.dialogAction.responseCard = createCard(getStageCards("What stage would you like me to lookup?"));
+            response.dialogAction.message = null;
+        } else if(slot_to_elicit === "sales" || slot_to_elicit === "salesOne" || slot_to_elicit === "salesTwo") {
+            if(slot_to_elicit === "salesOne") {
+                response.dialogAction.responseCard = createCard(getSalesCards("What sales person would you like me to lookup first?"));
+            } else if(slot_to_elicit === "salesTwo") {
+                response.dialogAction.responseCard = createCard(getSalesCards("What sales person would you like me to compare the first with?"));
+            } else {
+                response.dialogAction.responseCard = createCard(getSalesCards("What sales person would you like me to lookup?"));
+            }
+
+            response.dialogAction.message = null;
+        } else if(slot_to_elicit === "confirmation") {
+            response.dialogAction.responseCard = createCard(getConfirmationCards(message));
+            response.dialogAction.message = null;
+        } else if(slot_to_elicit === "engagements") {
+            response.dialogAction.responseCard = createCard(getEngagmentCards("What engagement would you like me to lookup?"));
+            response.dialogAction.message = null;
+        } else if(slot_to_elicit === "timeframe") {
+            response.dialogAction.responseCard = createCard(getTimeframeCards("What timeframe would you like me to lookup?"));
+            response.dialogAction.message = null;
+        }
+
+        console.log(`Lex Response: ${JSON.stringify(response)}`);
+
+        callback(null, response);
     } else {
         console.log("Validating To Alexa");
 
         event.request.intent.slots[slot_to_elicit].confirmationStatus = "NONE";
 
-        callback(null, {
+        response = {
             version : "1.0",
             sessionAttributes : {},
             response : {
@@ -153,6 +253,10 @@ exports.processValidation = (callback, event, slot_to_elicit, message) => {
                     }
                 ]
             },
-        });
+        };
+
+        console.log(`Alexa Response: ${JSON.stringify(response)}`);
+
+        callback(null, response);
     }
 };
